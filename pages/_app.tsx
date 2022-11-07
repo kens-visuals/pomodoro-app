3d452d144e6b1eec4aa4e535736c2e5c1d8b6173
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-11-06 22:49:25 -0800

    add context provider and StylesList

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+// Context
+import { StyleProvider } from '../contexts/StyleContext';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <StyleProvider>
+      <Component {...pageProps} />
+    </StyleProvider>
+  );
 }
