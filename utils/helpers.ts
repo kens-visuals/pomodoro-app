238006e89d08bb9cc39d3eb83542d56c7598c98c
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-11-07 20:41:52 -0800

    add helper funcs

---
+++ utils/helpers.ts
@@ -0,0 +1,9 @@
+export const padTo2Digits = (num: number): string =>
+  num.toString().padStart(2, '0');
+
+export const convertSecondsToMinutes = (remainingTime: number): string => {
+  const minutes = Math.floor(remainingTime / 60);
+  const extraSeconds = remainingTime % 60;
+
+  return `${padTo2Digits(minutes)}:${padTo2Digits(extraSeconds)}`;
+};
