6655e454f6e0ce7b96ad6ae256589ca7bdee829d
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-11-11 14:59:32 -0800

    add TimerToggler

---
+++ components/TimerToggler.tsx
@@ -0,0 +1,61 @@
+import { useContext, useEffect } from 'react';
+import { RadioGroup } from '@headlessui/react';
+
+// Context
+import { StyleContext } from '../contexts/StyleContext';
+import { TimerContext } from '../contexts/TimerContext';
+
+interface TimeOptionTypes {
+  id: number;
+  name: string;
+  value: string;
+}
+
+export default function TimerToggler() {
+  const { color } = useContext(StyleContext);
+  const {
+    timeOption,
+    setTimeOption,
+    handleStopClick,
+    handleResetClick,
+    timerDuration,
+  } = useContext(TimerContext);
+
+  const activeColor: string =
+    // eslint-disable-next-line no-nested-ternary
+    color === 'red' ? 'bg-red' : color === 'cyan' ? 'bg-cyan' : 'bg-violet';
+
+  const timeOptions: TimeOptionTypes[] = [
+    { id: 1, name: 'pomodoro', value: 'pomodoro' },
+    { id: 2, name: 'short break', value: 'shortBreak' },
+    { id: 3, name: 'long break', value: 'longBreak' },
+  ];
+
+  useEffect(() => {
+    handleResetClick();
+    handleStopClick();
+  }, [timerDuration, timeOption]);
+
+  return (
+    <RadioGroup
+      value={timeOption}
+      onChange={setTimeOption}
+      className='m-11 grid w-full grid-cols-3 items-center justify-between gap-4 rounded-full bg-primary-dark p-2 text-body-2 tracking-wide'
+    >
+      {timeOptions.map(({ id, name, value }) => (
+        <RadioGroup.Option value={value} key={id} className='w-full'>
+          {({ checked }) => (
+            <button
+              type='button'
+              className={`flex w-full items-center justify-center rounded-full py-4 text-center  ${
+                checked ? `${activeColor} text-primary-dark` : `text-tertiary`
+              }`}
+            >
+              {name}
+            </button>
+          )}
+        </RadioGroup.Option>
+      ))}
+    </RadioGroup>
+  );
+}
