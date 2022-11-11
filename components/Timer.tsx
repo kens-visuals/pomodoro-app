8f8232ba5aaaf8dadafce3a122bc49038b36a901
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-11-11 15:03:14 -0800

    fix Timer

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,21 +1,26 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 
 // Context
 import { StyleContext } from '../contexts/StyleContext';
+import { TimerContext } from '../contexts/TimerContext';
 
 // Components
 import TimerDisplay from './TimerDisplay';
 
+// Utils
+import { convertMinutesToSeconds } from '../utils/helpers';
+
 export default function Timer() {
   const { color } = useContext(StyleContext);
-
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [resetKey, setResetKey] = useState(0);
+  const { isPlaying, resetKey, handleOnComplete, timerDuration, timeOption } =
+    useContext(TimerContext);
 
   const activeColor: string =
     // eslint-disable-next-line no-nested-ternary
     color === 'red' ? 'F87070' : color === 'cyan' ? '70F3F8' : 'D881F8';
+
+  const convertedDuration = convertMinutesToSeconds(+timerDuration[timeOption]);
 
   return (
     <div className='my-12 flex aspect-square w-full items-center justify-center rounded-full bg-primary-gradient p-4 shadow-primary-shadow'>
@@ -24,18 +29,14 @@ export default function Timer() {
           key={resetKey}
           rotation='counterclockwise'
           isPlaying={isPlaying}
-          duration={10}
+          duration={convertedDuration}
           colors={`#${activeColor}`}
           trailColor='#161932'
           size={300}
+          onComplete={handleOnComplete}
         >
           {({ remainingTime }) => (
-            <TimerDisplay
-              isPlaying={isPlaying}
-              setResetKey={setResetKey}
-              setIsPlaying={setIsPlaying}
-              remainingTime={remainingTime}
-            />
+            <TimerDisplay remainingTime={remainingTime} />
           )}
         </CountdownCircleTimer>
       </div>
