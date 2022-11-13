424aadd2378b20e4b491bfb9d69b8f4f2449186a
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-11-13 11:59:37 -0800

    add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className='font-roboto my-4 text-center text-[.6rem] text-tertiary md:text-xs'>
+      Coded by{' '}
+      <a
+        href='https://www.ken.engineer/'
+        target='_blank'
+        rel='noreferrer'
+        className='font-bold uppercase underline transition-all duration-300 hover:text-secondary-dark'
+      >
+        Ken
+      </a>
+    </footer>
+  );
+}
